1247bc4142fcdb2dacfe29ef980727c858606815
Kevin Smith <kesmith@backcountry.com>
2014-07-11 17:13:38 -0600

    WIP - angular and firebase sandbox with airport data

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
-var app = angular.module('airportApp', []);
+var app = angular.module('airportApp', ['firebase']);
 
-app.controller('AirportController', ['$scope', function($scope) {
+app.controller('AirportController', ['$scope', 'AirportDelayService', function($scope, airportDelayService) {
+    var getAirportData = function(airportCode) {
+        $scope.airport = airportDelayService;
+        console.log('airport', $scope.airport)
+    };
 
+    getAirportData('SLC');
+
+    $scope.getAirport = function(airportCode) {
+        // Clear out all old data
+        $scope.airport = null;
+        getAirportData(airportCode);
+    };
+}]);
+
+app.factory('AirportDelayService', ['$firebase', function($firebase) {
+    var airportsRef = new Firebase("https://publicdata-airports.firebaseio.com/");
+    airportsRef.child("SFO").on("value", delayInfo);
+
+    function delayInfo(snapshot) {
+        var airport = snapshot.val();
+        console.log("Delay: " + airport.delay + " reason: " + airport.status.reason);
+    }
+
+    return $firebase(airportsRef);
 }]);
