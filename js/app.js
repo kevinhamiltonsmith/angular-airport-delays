892a1eff90d4226160f3abc1bd168b7478821097
Kevin Smith <kesmith@backcountry.com>
2014-07-14 17:29:24 -0600

    showing all airport data, color coded, with live filtering

--- js/app.js
+++ js/app.js
@@ -1,16 +1,25 @@
 var app = angular.module('airportApp', ['firebase']);
 
 app.controller('AirportController', ['$scope', '$firebase', function($scope, $firebase) {
+    // Get data for a single airport
     $scope.getAirportData = function(airportCode) {
         var url = 'https://publicdata-airports.firebaseio.com/';
         $scope.data = $firebase(new Firebase(url + airportCode));
     };
-}]);
 
-app.directive('prevent-default', function() {
-    return function(scope, element, attrs) {
-        $(element).click(function(event) {
-            event.preventDefault();
+    // Get all airport data
+    $scope.getAllAirports = function() {
+        var airportsObj = $firebase(new Firebase('https://publicdata-airports.firebaseio.com/'));
+        $scope.airports = [];
+
+        // Convert Firebase object into an array of objects, so angular filter will work
+        airportsObj.$on('loaded', function() {
+            var keys = airportsObj.$getIndex();
+            angular.forEach(keys, function(key) {
+                if (key !== '_credits' && key !== '_updated') {
+                    $scope.airports.push(airportsObj[key]);
+                }
+            });
         });
     };
-});
+}]);
